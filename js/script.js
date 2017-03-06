f4c7cdd51f9ea510b09bd6a2eb64424d3fcd00da
unknown <estudiante@LAB0262.SERVERINT.LOCAL>
2017-03-06 11:51:41 -0600

    sat

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var mario = angular.module('mario', []);
+BryanPaz.controller('Operaciones', function($scope) {
+$scope.precio = 50
+$scope.iva = 0.12
+$scope.precio = $scope.precio
+  $scope.iva = $scope.precio * $scope.iva;
+}); 
